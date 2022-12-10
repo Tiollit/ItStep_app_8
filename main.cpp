8a6cf95ae8de9161c71c8bfc33fb437dd97d0abb
Tiollit <Tiollit7@gmail.com>
2022-12-10 06:54:03 -0800

    h

--- main.cpp
+++ main.cpp
@@ -3,3 +3,30 @@
 #include <string.h>
 using namespace std;
 
+int main()
+{
+	char str1[500];
+	char str2[500];
+	cout << "Incert speech: ";
+	gets_s(str1);
+	cout << "Incert a word: ";
+	gets_s(str2);
+
+	for (int i = 0; i < strlen(str1); i++)
+	{
+		char* word = strstr(str1, str2);
+		if (*word != 0)
+		{
+			strcpy(word, str2);
+		}
+	}
+	cout << "New speech: " << endl;
+	cout << str1;
+
+	for (int i = 0; i < strlen(str1); i++)
+	{
+		if (str1[i] == ".")		
+	}
+
+	return 0;
+}
