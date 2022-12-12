0f85a17394c89b40b9a378c06457f1f2bcd5be5a
Tiollit <Tiollit7@gmail.com>
2022-12-12 14:29:06 -0800

    j

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
 	char str[500] = "Appends a copy of the source string to the destination string. The terminating null character in destination is overwritten by the first character of source, and a null-character is included at the end of the new string formed by the concatenation of both in destination.";
+	cout << "Incert a looking word: ";
 
 
 	return 0;
