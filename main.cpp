503afd53d54bf5e79587a3778940efa9d44adb61
Tiollit <Tiollit7@gmail.com>
2022-12-12 14:28:34 -0800

    h

--- main.cpp
+++ main.cpp
@@ -3,3 +3,10 @@
 #include <string.h>
 using namespace std;
 
+int main()
+{
+	char str[500] = "Appends a copy of the source string to the destination string. The terminating null character in destination is overwritten by the first character of source, and a null-character is included at the end of the new string formed by the concatenation of both in destination.";
+
+
+	return 0;
+}
