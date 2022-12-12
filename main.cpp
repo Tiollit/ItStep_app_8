f094b49c9aa3b770234c5934c133e2407fcd2607
Tiollit <Tiollit7@gmail.com>
2022-12-12 14:21:17 -0800

    H

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//_CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 #include <string.h>
@@ -5,7 +6,17 @@ using namespace std;
 
 int main()
 {
+	char str[100] = "This is test version of C++. 1234567890.";
+	char Num[100] = "1234567890";
+	char New[100];
 
+	cout << "Lenght of str is: " << strlen(str) << " Length of Num is: " << strlen(Num) << endl;
+	strcpy(New, str);
+	cout << "Copy of str is: " << New << endl;
 
-return 0;
+	strcat(New, Num);
+	cout << "Combining New and Num arrays: " << New << endl;
+	cout << "First time symbol C meets in New array is: " << strchr(New, 'C') << endl;
+	cout << "First time Num meets in New array is: " << strstr(New, Num) << endl;
+    return 0;
 }
