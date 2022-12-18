499db32a8e427571b82846ddb8b89bdbadb7acc1
Tiollit <Tiollit7@gmail.com>
2022-12-18 11:39:17 -0800

    h

--- main.cpp
+++ main.cpp
@@ -3,9 +3,46 @@
 #include <string.h>
 using namespace std;
 
-int main()
+int LookForWords(const char* text, const char* word)
 {
+	bool triger = true;
+	int n = 0;
+	uint32_t index;
+	for (size_t i = 0, j = 0; i < strlen(text); i++)
+	{
+		triger = true;
+		if (text[i] == word[j])
+		{
+			index = i;
+			for (; j < strlen(word); j++)
+			{
+				if (text[index] != word[j])
+				{
+					triger = false;
+					break;
+				}				
+				index++;
+				if (triger != false)
+				{
+					n += 1;
+					i = index - 1;
+				}
+				else j = 0;
+			}
+		}
+	}
+	return n;
 
+}
 
-	return 0;
+int main()
+{	
+string text, word, new_text_reverse, new_text_reversespeech;	
+
+cout << "Insert speech: " << endl;
+getline(cin, text);
+cout << "Insetr word: " << endl;
+getline(cin, word);
+
+return 0;  
 }
