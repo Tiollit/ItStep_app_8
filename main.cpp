e53c32605d558471b77fe014db1e76bd667720cd
Tiollit <Tiollit7@gmail.com>
2022-12-19 08:38:21 -0800

    j

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 	int a;
-	char str1[500], str2[100];
+	char str1[500], str2[500];
 	char str3[] = "1234567890";
 	cout << "Incert 2 speeches: ";
 	cout << "Speech 1:";
@@ -15,13 +15,15 @@ int main()
 	gets_s(str2);
 
 	if (strcmp(str1, str2) == 0) cout << "Speeches the same";
-	else if (strcmp(str1, str2) == 1) cout << "str1 more then str2";
+	else if (strcmp(str1, str2) == 1) cout << "str1 more then str2" << endl;
 	else cout << "str2 more then str1";
-	a = StringToNumber(str3);                         // функція конвертує рядок у число та повертає це число
+	char* StringToNumber(char *str3);                 // функція конвертує рядок у число та повертає це число
 	char* NumberToString(int number);                 // функція конвертує число в рядок і повертає покажчик на цей рядок
-	char* Uppercase(char* str1);                      // функція перетворює рядок у верхній регістр
-	char* Lowercase(char* str1);                      // функція перетворює рядок у нижній регістр
-	char* mystrrev(char* str);                        // функція реверсує рядок і повертає покажчик на новий рядок
-	 
+	toupper(*str1);                      // функція перетворює рядок у верхній регістр
+	cout << "Upper Case str1: " << str1 << endl;
+    tolower(*str2);                      // функція перетворює рядок у нижній регістр
+	cout << "Lower Case str2: " << str2 << endl;
+	_strrev(str3);                                     // функція реверсує рядок і повертає покажчик на новий рядок
+	cout << "Reverse of str3: " << str3 << endl;
 	return 0;
 }
