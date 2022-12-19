9a46aae28e7a7031edf97e2a566684a6ea3fb982
Tiollit <Tiollit7@gmail.com>
2022-12-19 09:11:04 -0800

    f

--- main.cpp
+++ main.cpp
@@ -34,10 +34,22 @@ int LookForWords(const char* text, const char* word)
 
 int NofSpeeches(const char* text)
 {
+	int n = 0;
 	for (size_t i = 0; i < strlen(text); i++)
 	{
-      
+		if (text[i] == '.' || text[i] == '!' || text[i] == '?' && text[i+1] != '.') n += 1;
     }
+	cout << "Number of speeches in text: " << n << endl;
+}
+
+int NofSymbols(const char* text)
+{
+	int n = 0;
+	for (size_t i = 0; i < strlen(text); i++)
+	{
+		if (text[i] == '.' || text[i] == ',') n += 1;
+	}
+	cout << "Number of symbols '.' and ',' in text: " << n << endl;
 }
 
 string reverse(const char* text, char* new_text_reverse)
@@ -71,8 +83,10 @@ cout << "Insert word: " << endl;
 getline(cin, word);
 int n = LookForWords(text.c_str(), word.c_str());
 cout << "The number of word: " << word << " in text is: " << n << endl;
-string reverse(text.c_str(), new_text_reverse.c_str());
-string reverse_2(text.c_str(), new_text_reversespeech.c_str());
+reverse(text.c_str(), new_text_reverse.c_str());
+reverse_2(text.c_str(), new_text_reversespeech.c_str());
+NofSpeeches(text.c_str());
+NofSymbols(text.c_str());
 
 return 0;  
 }
